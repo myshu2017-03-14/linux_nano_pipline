e68a933db14999428ee0f950eca2ccd450ad1c7d
Myshu <1291016966@qq.com>
2018-05-23 14:47:12 +0800

    update

--- 16S_and_ITS_data_analysis/blastn.sh
+++ 16S_and_ITS_data_analysis/blastn.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 #---------------------------------------------------------------+
 #     author: Myshu                                             |
-#			mail:1291016966@qq.com                                    |
+#     mail:1291016966@qq.com                                    |
 #     version:1.0                                               |
 #     date :2018-5-3                                            |
 #     description: run blast for 16S and ITS                    |
@@ -11,10 +11,14 @@
 help()
 {
 cat <<HELP
-USAGE: $0 fasta_dir fasta_len_dir
+USAGE: $0 fasta_dir fasta_len_dir tag out_dir
     or $0 ‐h # show this message
 EXAMPLE:
-    $0 test/ test/
+    $0 test/ test/ -c blastn_out
+    Tag : -a for 16S
+	  -b for ITS
+	  -c for 16S+ITS
+
 HELP
 exit 0
 }
@@ -24,24 +28,34 @@ exit 0
 IN_DIR=$1 # input fasta file
 IN_LEN=$2 # input length file of fasta
 pro=$(dirname $0)
-if [ ! -d "blastn_out" ]; then
-	mkdir blastn_out
+tag=$3
+out=$4
+if [ ! -d $out ]; then
+	mkdir $out
 fi
-cd blastn_out
-for i in $IN_DIR/BC*.fasta
-do
-	name=$(basename $i .fasta)
-	# 16S
-	blastn -query $i -db $pro/database/16S_db/NCBI-16S-19088 -out $name\_blastn_16S.out -outfmt 6 -evalue 1e-5 -num_threads 6 -max_target_seqs 5
-	perl $pro/db_program/result2anno.pl -in $name\_blastn_16S.out -tab $pro/database/16S_db/NCBI-16S-19088.tab -out $name\_blastn_16S_anno.out
-	perl $pro/db_program/calculate_coverage_for_blast_results.pl -in $name\_blastn_16S_anno.out -len $IN_LEN/$name.len -out $name\_blastn_16S_anno_cov.out
-	perl $pro/db_program/get_one_results_of_each.pl -in $name\_blastn_16S_anno_cov.out -out $name\_blastn_16S_anno_cov_uniq.out
-
-	# ITS
-	blastn -query $i -db $pro/database/ITS_db/fungi.ITS.fna -out $name\_blastn_ITS.out -outfmt 6 -evalue 1e-5 -num_threads 6 -max_target_seqs 5
-	perl $pro/db_program/result2anno.pl -in $name\_blastn_ITS.out -tab $pro/database/ITS_db/fungi.ITS.tab -out $name\_blastn_ITS_anno.out
-	perl $pro/db_program/calculate_coverage_for_blast_results.pl -in $name\_blastn_ITS_anno.out -len $IN_LEN/$name.len -out $name\_blastn_ITS_anno_cov.out
-	perl $pro/db_program/get_one_results_of_each.pl -in $name\_blastn_ITS_anno_cov.out -out $name\_blastn_ITS_anno_cov_uniq.out
-
-
-done	
+# for 16S
+if [ "$tag" = "-a" ] || [ "$tag" = "-c" ]; then
+	for i in $IN_DIR/BC*.fasta
+	do
+		name=$(basename $i .fasta)
+		# 16S
+		blastn -query $i -db $pro/database/16S_db/NCBI-16S-19088 -out $out/$name\_blastn_16S.out -outfmt 6 -evalue 1e-5 -num_threads 6 -max_target_seqs 5
+		perl $pro/db_program/result2anno.pl -in $out/$name\_blastn_16S.out -tab $pro/database/16S_db/NCBI-16S-19088.tab -out $out/$name\_blastn_16S_anno.out
+		perl $pro/db_program/calculate_coverage_for_blast_results.pl -in $out/$name\_blastn_16S_anno.out -len $IN_LEN/$name.len -out $out/$name\_blastn_16S_anno_cov.out
+		perl $pro/db_program/get_one_results_of_each.pl -in $out/$name\_blastn_16S_anno_cov.out -out $out/$name\_blastn_16S_anno_cov_uniq.out
+	done
+fi
+# for ITS
+if [ "$tag" = "-b" ] || [ "$tag" = "-c" ]; then
+	for i in $IN_DIR/BC*.fasta
+	do
+		# ITS
+		name=$(basename $i .fasta)
+		blastn -query $i -db $pro/database/ITS_db/fungi.ITS.fna -out $out/$name\_blastn_ITS.out -outfmt 6 -evalue 1e-5 -num_threads 6 -max_target_seqs 5
+		perl $pro/db_program/result2anno.pl -in $out/$name\_blastn_ITS.out -tab $pro/database/ITS_db/fungi.ITS.tab -out $out/$name\_blastn_ITS_anno.out
+		perl $pro/db_program/calculate_coverage_for_blast_results.pl -in $out/$name\_blastn_ITS_anno.out -len $IN_LEN/$name.len -out $out/$name\_blastn_ITS_anno_cov.out
+		perl $pro/db_program/get_one_results_of_each.pl -in $out/$name\_blastn_ITS_anno_cov.out -out $out/$name\_blastn_ITS_anno_cov_uniq.out
+	
+	
+	done
+fi	
